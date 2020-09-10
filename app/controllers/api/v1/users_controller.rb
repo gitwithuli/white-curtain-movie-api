7bd3a10d48d2779c9e6ac30277600e7557418d58
Ulas <uli.will.code@protonmail.com>
2020-09-10 23:45:01 +0300

    login logic completed

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:login, :register]
+
+  def register
+    user = User.create(user_params)
+    if user.valid? && user.save
+      render json: user,
+        status: 201
+      return
+    end
+    render json: user.errors,
+      status: 401
+  end
+
+  def login
+    email = params[:user][:email]
+    password = params[:user][:password]
+    user = User.find_by(email: email)
+    is_valid = user && user.valid_password?(password)
+    unless is_valid
+      render json: {
+        status: 'error',
+        message: 'Invalid credentials'
+      }, status: 400 and return
+    end
+    payload = {user_id: user.id}
+    access_token = AccessToken.encode(payload)
+    return render json: user,
+      meta: {access_token: access_token},
+      status: 200
+  end
+
+  def test
+    render json: @current_user, status: 200
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation,
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # devise_for :users, controllers: { sessions: 'sessions', registrations: 'api/v1/registrations' }
+      # post 'login' => 'user_token#create'
+      # resources :users, only: [:create]
+      # get 'users/me', to: 'users#me'
+      #  get 'movies/recommendations', to: 'movies#get_recommendations'
+
+      # resources :movies, only: [ :index, :show ]do
+      #   post 'follow' => 'movies#follow'
+      #   delete 'unfollow' => 'movies#unfollow'
+      # end
+
+
+      # resources :genres, only: [ :index, :show ]do
+      #   post 'follow' => 'genres#follow'
+      #   delete 'unfollow' => 'genres#unfollow'
+      # end
+
+      # resources :stars, only: [ :index, :show ]do
+      #   post 'follow' => 'stars#follow'
+      #   delete 'unfollow' => 'stars#unfollow'
+      # end
+      post 'users/register', to: 'users#register'
+      post 'users/login', to: 'users#login'
+      get 'users/test', to: 'users#test'
+    end
+  end
+ end
