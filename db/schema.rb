d4f0a819d260aee356d562e6e9828468e704919b
Ulas <uli.will.code@protonmail.com>
2020-09-11 14:45:10 +0300

    models are created

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_192843) do
+ActiveRecord::Schema.define(version: 2020_09_11_103609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.string "followable_type"
+    t.bigint "followable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id"
+    t.index ["user_id"], name: "index_follows_on_user_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.string "description"
+    t.string "poster"
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_movies_on_genre_id"
+  end
+
+  create_table "starrings", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "star_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_starrings_on_movie_id"
+    t.index ["star_id"], name: "index_starrings_on_star_id"
+  end
+
+  create_table "stars", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +67,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_192843) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "movies", "genres"
+  add_foreign_key "starrings", "movies"
+  add_foreign_key "starrings", "stars"
 end
